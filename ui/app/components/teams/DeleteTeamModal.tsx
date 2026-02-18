a99fbb9fadf84e7212bd854b2ac19ea6f5ee1eb7
Kamisia <kamila.samczuk@gmail.com>
2026-02-18 10:45:32 +0100

    refactor(teams): migrate DeleteTeamModal layout to Strato components

--- ui/app/components/teams/DeleteTeamModal.tsx
+++ ui/app/components/teams/DeleteTeamModal.tsx
@@ -1,9 +1,12 @@
-﻿import React from "react";
-import { Modal } from "@dynatrace/strato-components-preview/overlays";
-import { Button } from "@dynatrace/strato-components/buttons";
+import React from "react";
+
 import { useDeleteSettingsV2 } from "@dynatrace-sdk/react-hooks";
-import { Team } from "app/utils/teams/types";
+import { Button } from "@dynatrace/strato-components/buttons";
+import { Flex } from "@dynatrace/strato-components/layouts";
+import { Text } from "@dynatrace/strato-components/typography";
+import { Modal } from "@dynatrace/strato-components-preview/overlays";
 import { useIntl } from "react-intl";
+import { Team } from "app/utils/teams/types";
 import { teamsMessages } from "./messages";
 
 interface Props {
@@ -15,35 +18,37 @@ interface Props {
 export function DeleteTeamModal({ team, closeDialog, afterDelete }: Props) {
   const intl = useIntl();
   const { execute } = useDeleteSettingsV2();
+
   const onDelete = async () => {
     await execute({
-        objectId: team.id,
-        optimisticLockingVersion: team.version,
-      });
+      objectId: team.id,
+      optimisticLockingVersion: team.version,
+    });
     await afterDelete();
   };
+
   return (
     <Modal
       title={intl.formatMessage(teamsMessages.deleteTeamTitle)}
       show={true}
       onDismiss={closeDialog}
     >
-      <div style={{ display: "flex", flexDirection: "column", gap: 16 }}>
-        <div>
+      <Flex flexDirection="column" gap={16}>
+        <Text>
           {intl.formatMessage(teamsMessages.deleteTeamConfirmation, {
             teamName: team.name,
           })}
-        </div>
+        </Text>
 
-        <div style={{ display: "flex", justifyContent: "flex-end", gap: 8 }}>
+        <Flex justifyContent="flex-end" gap={8}>
           <Button onClick={closeDialog}>
             {intl.formatMessage(teamsMessages.cancelButton)}
           </Button>
           <Button onClick={() => void onDelete()}>
             {intl.formatMessage(teamsMessages.deleteButton)}
           </Button>
-        </div>
-      </div>
+        </Flex>
+      </Flex>
     </Modal>
   );
 }
